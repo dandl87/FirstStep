6be47f0387f588107b2441637d89ab6bea9cc6ab
Daniele De Lorenzo <dlorenzo.daniele@gmail.com>
2020-12-15 15:00:16 +0100

    Primo commit... input da file della radice

--- firstStep/src/firstStep/RandomApp.java
+++ firstStep/src/firstStep/RandomApp.java
@@ -1,17 +1,29 @@
 package firstStep;
+ 
+import java.io.IOException;
+import java.util.Scanner; 
 
 public class RandomApp {
 	
 	public static void main(String[] args) {
-		for(int i=0; i<100;i++) {
-		int x = ((int) (100*Math.random())%50);
-		System.out.println(x);
+		System.out.println("inserire range di valori possibili");
+		Scanner input = new Scanner(System.in);
+		int max  = input.nextInt();
+		 
 		try {
+			 
+			System.out.println("max:" +max);
+			System.out.println("x:" +max*Math.random());
+		for(int i=0; i<10;i++) {
+		int x = ((int) (100*Math.random())%max);
+		System.out.println(x);
+		}
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}finally {
+			input.close();
 		}
 	}
 	
